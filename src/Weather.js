7fed2c089bd5c27cd5e72d8f2ad88c4186f9fd48
fel <felite333@gmail.com>
2022-08-20 16:55:50 +0200

    fin6

--- src/Weather.js
+++ src/Weather.js
@@ -125,25 +125,25 @@ export default function Weather(props) {
 
         <footer>
           <small>
-            This project was coded by{" "}
+            This project was coded by
             <a
               href="https://www.linkedin.com/in/liubov-shupik-46940016b/"
               target="_blank"
               rel="noreferrer"
             >
               Liubov Shupik
-            </a>{" "}
-            and is{" "}
+            </a>
+            and is
             <a
-              href="https://github.com/fel562/weather-r"
+              href="https://github.com/fel562/weather-forecast"
               target="_blank"
               rel="noreferrer"
             >
               open-sourced on GitHub
-            </a>{" "}
-            and{" "}
+            </a>
+            and
             <a
-              href="https://github.com/fel562/weather-forecast"
+              href="https://weather-forecast-liubov.netlify.app/"
               id="testlink"
               target="_blank"
               rel="noreferrer"
